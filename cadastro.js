d9005f99e199aa3ad21451895a183717da3482c7
NinicolasGTH <nic.macedo2012@gmail.com>
2024-09-25 20:02:09 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -4,9 +4,25 @@ async function cadastroUsuario() {
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    let cpf_cnpj = document.getElementById('cpf_cnpj').value;
+    let cpf_cnpj = document.getElementById('cpf').value;
     let terms = document.getElementById('terms').checked;
     let birthday = document.getElementById('birthday').value;
+
+    if (password.length < 6) {
+        alert("A senha deve ter no mínimo 6 caracteres");
+        return;
+    }
+
+    // verificação dos dados que estão sendo informados
+    console.log({
+        "name": name,
+        "email": email,
+        "user_type_id": 1,
+        "password": password,
+        "cpf_cnpj": cpf_cnpj,
+        "terms": terms,
+        "birthday": birthday 
+    });
 
     let api = await fetch(url, {
         method: "POST",
@@ -27,14 +43,20 @@ async function cadastroUsuario() {
     if (api.ok) {
         let resposta = await api.json();
         console.log(resposta);
-        console.log("Cadastro realizado com sucesso!");
         alert("Cadastro realizado com sucesso! Enviamos um link de verificação no seu email");
     } else {
         let respostaErro = await api.json();
-        
-        if (respostaErro.data.errors) {
-            console.log(respostaErro.data.errors.cpf_cnpj, respostaErro.data.errors.email, respostaErro.data.errors.password);
+        console.log(respostaErro); // mostrando o possível erro que pode se dar. Mostrado no console
+
+        if (respostaErro.data && respostaErro.data.errors) {
+            if (respostaErro.data.errors.email) {
+                alert(`Erro: O email ${email} já está em uso`);
+            }
+            if (respostaErro.data.errors.cpf_cnpj) {
+                alert(`Erro: O CPF/CNPJ ${cpf_cnpj} já está em uso`);
+            }
+        } else {
+            alert("Erro ao realizar o cadastro. Tente novamente.");
         }
-        alert("Erro");
     }
-    }
+}
